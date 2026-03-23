b9ca37697b26fb0e7b6d5c92802e01649757d76c
Aidan Roe-Briesewitz <aidanroebriesewitz@gmail.com>
2026-03-23 17:39:52 +0000

    Made card with text boxes to input club info, along with submit button, needs customization work, will be done tomorrow

--- app/clubs/create/page.tsx
+++ app/clubs/create/page.tsx
@@ -1,3 +1,6 @@
+import { Button } from "@/components/ui/button";
+import {Input} from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import {
   Field,
   FieldContent,
@@ -10,20 +13,59 @@ import {
   FieldSet,
   FieldTitle,
 } from "@/components/ui/field";
-import {Input} from "@/components/ui/input"
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
 export default function FieldInput() {
     return (
-        <FieldSet className = "w-[700px] max-w-l">
-          <FieldGroup>
-            <Field>
-              <FieldLabel htmlFor="Club Name">Club Name</FieldLabel>
-              <Input id="CName" type="text" placeholder="Insert Name(e.g. Falkon Robotics)"/>
-              <FieldLabel htmlFor="Description">Description</FieldLabel>
-              <Input id="Des" type="text" placeholder="Insert Description(e.g. Build stuff)"/>
-              <FieldLabel htmlFor="Room Number">Room Number</FieldLabel>
-              <Input id="Num" type="text" placeholder="Insert Room Number"/>
-            </Field>
-          </FieldGroup>
-        </FieldSet>
+      <div className="flex items-center justify-center min-h-screen">
+      <Card className="w-full max-w-lg md:max-w-2xl p-6">
+        <CardTitle>Create a Club</CardTitle>
+        <form>
+        <div className="flex flex-col gap-6 p-3">
+          <div className="grip gap-2">
+            <div className="flex flex-col gap-2">
+            <Label htmlFor="Club Name" className="flex gap-4">Enter Club Name</Label>
+            <Input 
+            id="Club Name"
+            type="Club Name"
+            placeholder="ex. Falkon Robotics"
+            required
+            />
+            <div className="flex flex-col gap-2">
+            <Label htmlFor="Club Description" className="flex gap-4">Enter Club Description</Label>
+            <Input 
+            id="Club Description"
+            type="Club Description"
+            placeholder="ex. You have a lot of fun"
+            required
+            />
+            </div>
+            <div className="flex flex-col gap-2">
+            <Label htmlFor="Room Number" className="gap-4">Enter Room Number</Label>
+            <Input 
+            id="Room Number"
+            type="Room Number"
+            placeholder="ex. 1308"
+            required
+            />
+            </div>
+          </div>
+          </div>
+        </div>
+        </form>
+        <CardFooter className="flex-col gap-2">
+        <Button type="submit">
+          Submit Club
+        </Button>
+      </CardFooter>
+      </Card>
+      </div>
     )
 }
