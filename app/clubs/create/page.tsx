8bd098b76a397dfd76df22e28286cdbd76fa5a35
Aidan Roe-Briesewitz <aidanroebriesewitz@gmail.com>
2026-04-02 16:07:10 +0000

    Working upload image button(data now needs to be stored backend), added text column for club links

--- app/clubs/create/page.tsx
+++ app/clubs/create/page.tsx
@@ -1,6 +1,10 @@
+"use client"
 import { Button } from "@/components/ui/button";
 import {Input} from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import {IconPaperclip} from "@tabler/icons-react";
+import {launchImageLibrary} from "react-native-image-picker";
+import {useRef} from "react";
 import {
   Field,
   FieldContent,
@@ -22,7 +26,17 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
+const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+const file = event.currentTarget.files?.[0]
+if(file){
+console.log(file)
+}
+}
 export default function FieldInput() {
+  const fileInputRef = useRef<HTMLInputElement>(null)
+  const handleClick = () => {
+    fileInputRef.current?.click()
+  }
     return (
       <div className="flex items-center justify-center min-h-screen">
       <Card className="w-full max-w-lg md:max-w-2xl p-6">
@@ -32,6 +46,14 @@ export default function FieldInput() {
           <div className="grip gap-2">
             <div className="flex flex-col gap-2">
             <Label htmlFor="Club Name" className="flex gap-4">Enter Club Name</Label>
+            <input
+            type="file"
+            accept="image/*"
+            ref={fileInputRef}
+            onChange={handleFileChange}
+            style={{display: "none"}}
+            required
+            />
             <Input 
             id="Club Name"
             type="Club Name"
@@ -55,14 +77,24 @@ export default function FieldInput() {
             placeholder="ex. 1308"
             required
             />
+            <Label htmlFor="Club Links" className="gap-4">Enter Club Links</Label>
+            <Input
+            id="Club Links"
+            type="Links"
+            placeholder="ex. A google classroom link"
+            required
+            />
             </div>
           </div>
           </div>
         </div>
         </form>
-        <CardFooter className="flex-col gap-2">
-        <Button type="submit">
-          Submit Club
+        <CardFooter className="flex-left gap-4">
+        <Button variant="outline" size="sm" onClick={handleClick}>
+          <IconPaperclip/>Upload Club Photo </Button>
+        <input type="file" ref={fileInputRef} style={{display:"none"}}/>
+        <Button variant="outline" type="submit">
+        Submit Club
         </Button>
       </CardFooter>
       </Card>
